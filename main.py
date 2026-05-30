6fb69806508bc13af8df39a70c68619572426abc
tsuyoshi64 <tsurutsuyoshi0604@gmail.com>
2026-05-30 10:19:43 +0700

    B: Assignment: Using token count methods and parser for user's input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,11 +10,24 @@ def main():
     if api_key is None:
         raise RuntimeError("environment variable was not found")
     client = genai.Client(api_key=api_key)
+    # promptss = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=args.user_prompt,
     )
-    print(response.text)
+    if response.usage_metadata is None:
+        raise RuntimeError("Failed requesting API")
+
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
